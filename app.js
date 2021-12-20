5592a33bb1d5f9204f393100221500cdc0a4062d
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2021-12-20 21:19:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,99 @@
+import {UIController,UIElement} from './controller/controller.js';
+
+
+const ui_controller = new UIController(document.body);
+
+/*MAIN SCREEN*/
+const mainScreen = new UIElement("div");
+const logo = new UIElement("img","./img/logo.png");
+const renderCocinero = new UIElement("img","./img/renderCocinero.png");
+const howToPlay = new UIElement("button","How to play");
+const startButton = new UIElement("button","Start");
+const prizesButton = new UIElement("button","Premios y bases");
+const top100Button = new UIElement("button","Top 100 ranking");
+const buttonGroup = new UIElement("div");
+const basesButton = new UIElement("button","Bases");
+const privacyButton = new UIElement("button","Privacidad");
+/*MAIN SCREEN*/
+const secondaryButton = new UIElement("button", "Toggle_Second");
+const secondaryScreen = new UIElement("div");
+const thirdButton = new UIElement("button","Toggle_Third")
+const thirdScreen = new UIElement("div");
+const button = new UIElement("button", "Toggle");
+
+mainScreen.addStyle(`
+    position:absolute;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:space-between;
+    width:100%;
+    height: 100vh;
+    min-height:-webkit-fill-available;
+    background-image:url('./img/pattern.png');
+    z-index:3;
+    `
+);
+secondaryScreen.addStyle(
+    `
+    position:absolute;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    width:100%;
+    height: 100vh;
+    min-height:-webkit-fill-available;
+    background-color:green;
+    z-index:2;
+    `
+);
+
+thirdScreen.addStyle(`
+    position:absolute;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    width:100%;
+    height: 100vh;
+    min-height:-webkit-fill-available;
+    background-color:yellow; 
+    z-index:1;
+    `
+);
+
+logo.addStyle(
+    `
+    width:20vh;
+    max-width:30%;
+    `
+);
+
+renderCocinero.addStyle(
+    `
+    width:50vh;
+    max-width:90%;
+    `
+);
+
+buttonGroup.addStyle(
+    `
+    width:100%;
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between
+    `
+)
+
+
+buttonGroup.addElement(basesButton,privacyButton);
+mainScreen.addElement(logo,renderCocinero,howToPlay,startButton,prizesButton,top100Button,buttonGroup)
+
+
+button.toggleElement(secondaryScreen);
+secondaryButton.toggleElement(secondaryScreen);
+thirdButton.toggleElement(thirdScreen);
+
+secondaryScreen.addElement(secondaryButton);
+thirdScreen.addElement(thirdButton);
+ui_controller.add(mainScreen);
+ui_controller.add(secondaryScreen,thirdScreen);
---
+++ controller/controller.js
@@ -0,0 +1,97 @@
+var css = `
+*{margin: 0;padding: 0;font-family: sans-serif;}
+.hidden{
+    display: none!important;
+}
+`,
+head = document.head || document.getElementsByTagName('head')[0],
+style = document.createElement('style');
+
+head.appendChild(style);
+style.appendChild(document.createTextNode(css));
+
+
+export class UIController{
+    constructor(body){
+        this.body = body;
+        this.mainElement = document.createElement("div");
+        this.onCreate()
+    }
+    onCreate(){
+        this.body.appendChild(this.mainElement);
+        return this.mainElement;
+    }
+    add(param){
+        for(var i = 0; i<arguments.length;i++){
+            this.mainElement.appendChild(arguments[i].element)
+        }
+    }
+    child(){
+        console.log(this.mainElement.childNodes)
+    }
+
+}
+
+export class UIElement{
+    constructor(element, text){
+        this.element = document.createElement(element);
+        this.text = text;
+        this.innerText = document.createTextNode(text)
+        this.onCreate()
+        
+    }
+    onCreate(){
+        if(this.text != null){
+            this.element.appendChild(this.innerText)
+        }else{
+            this.element.innerText = '';
+        }
+        switch(this.element.nodeName){
+            case "BUTTON":
+                //console.log("its a button");
+                /*this.element.style = `
+                max-width:128px;
+                min-width:64px;
+                padding:4px 8px 4px 8px`;*/
+                break;
+            case "DIV":
+                //console.log('its a div')
+                break;
+            case "IMG":
+                this.element.src = this.innerText.data;
+        }
+    }
+    hideElement(element){
+        this.element.addEventListener("click",()=>{
+            element.element.classList.add('hidden');
+        })
+    }
+    toggleElement(element){
+        this.element.addEventListener("click", ()=>{
+            if(element.element.classList.contains('hidden')){
+                element.element.classList.remove('hidden');
+            }else{
+                element.element.classList.add('hidden');
+            }
+        })
+    }
+    trigger(time,func){
+        this.element.addEventListener("click", ()=>{
+            if(arguments.length === 1){
+                arguments[0]()
+           }else if(arguments.length ===2){
+                setTimeout(() => {
+                    arguments[1]()
+            }, arguments[0]);
+           }
+        })   
+    }
+    addStyle(s){
+        this.element.style.cssText += s;
+    }
+    addElement(){
+        for(var i = 0; i<arguments.length;i++){
+            this.element.appendChild(arguments[i].element)
+        }
+    }
+}
